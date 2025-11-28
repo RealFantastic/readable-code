8e630afb3627f991747948d57bb4d95379237ed0
JungCodee <wjdghks5698@nexgrid.co.kr>
2025-11-28 16:42:36 +0900

    ## 섹션 4. 객체 지향 패러다임 - SRP(Single Responsibility Principle) 1. `main()` 함수는 프로그램을 실행하는 진입점 역할만 수행, Minesweeper 안에서 게임을 조작하고 게임을 굴리도록 책임을 이동 2. 콘솔에 출력하는 영역과, 사용자 입력이 받는 부분을 분리 3. 게임 여기저기서 BOARD를 사용하는 경우가 많음 -> BOARD의 책임이 큼 -> 별도의 GameBoard class로 분리

---
+++ src/main/java/cleancode/minesweeper/tobe/GameBoard.java
@@ -0,0 +1,145 @@
+package cleancode.minesweeper.tobe;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class GameBoard {
+    private static final int LAND_MINE_COUNT = 10;
+
+    private final Cell[][] board;
+
+    public GameBoard(int rowSize, int colSize) {
+        board = new Cell[rowSize][colSize];
+    }
+
+    public void flag(int rowIndex, int colIndex) {
+        Cell cell = findCell(rowIndex, colIndex);
+        cell.flag();
+    }
+
+    public void open(int rowIndex, int colIndex) {
+        Cell cell = findCell(rowIndex, colIndex);
+        cell.open();
+    }
+
+    public boolean isLandMineCell(int selectedRowIndex, int selectedColumnIndex) {
+        Cell cell = findCell(selectedRowIndex, selectedColumnIndex);
+        return cell.isLandMine();
+    }
+
+    public boolean isAllCellChecked() {
+        return Arrays.stream(board) //Stream<Cell[]>
+                .flatMap(Arrays::stream) //Stream<Cell>
+                .allMatch(Cell::isChecked);
+    }
+
+    public void initializeGame() {
+        int rowSize = board.length;
+        int colSize = board[0].length;
+        for (int row = 0; row < rowSize; row++) { //i, j 를 명확히 구분 가능한 row, col로 변경
+            for (int col = 0; col < colSize; col++) {
+                board[row][col] = Cell.create();
+            }
+        }
+
+        for (int i = 0; i < LAND_MINE_COUNT; i++) {
+            int landMineCol = new Random().nextInt(colSize);
+            int landMineRow = new Random().nextInt(rowSize);
+            Cell landMineCell = findCell(landMineRow, landMineCol);
+            landMineCell.turnOnLandMine();
+        }
+
+        for (int row = 0; row < rowSize; row++) {
+            for (int col = 0; col < colSize; col++) {
+                if (isLandMineCell(row, col)) {
+                    continue;
+                }
+                int count = countNearbyLandMines(row, col);
+                Cell cell = findCell(row, col);
+                cell.updateNearbyLandMineCount(count); // 각 위치에 확인한 지뢰 개수 입력
+            }
+        }
+    }
+    public int getRowSize() {
+        return board.length;
+    }
+    public int getColSize() {
+        return board[0].length;
+    }
+
+    public String getSign(int rowIndex, int colIndex) {
+        Cell cell = findCell(rowIndex, colIndex);
+        return cell.getSign();
+    }
+
+
+    private Cell findCell(int rowIndex, int colIndex) {
+        return board[rowIndex][colIndex];
+    }
+
+    public int countNearbyLandMines(int row, int col) {
+        int rowSize = getRowSize();
+        int colSize = getColSize();
+
+        int count = 0;
+        if (row - 1 >= 0 && col - 1 >= 0 && isLandMineCell(row - 1, col - 1)) { // 선택 지점의 왼쪽 위에 지뢰가 있는지 확인
+            count++;
+        }
+        if (row - 1 >= 0 && isLandMineCell(row - 1, col)) { // 선택 지점의 정윗방향에 지뢰가 있는지 확인
+            count++;
+        }
+        if (row - 1 >= 0 && col + 1 < colSize && isLandMineCell(row - 1, col + 1)) { // 선택 지점의 오른쪽 위에 지뢰가 있는지 확인
+            count++;
+        }
+        if (col - 1 >= 0 && isLandMineCell(row, col - 1)) { //선택 지점의 정왼쪽에 지뢰가 있는지 확인
+            count++;
+        }
+        if (col + 1 < colSize && isLandMineCell(row, col + 1)) { // 선택 지점의 정 오른쪽 방향에 지뢰가 있는지 확인
+            count++;
+        }
+        if (row + 1 < rowSize && col - 1 >= 0 && isLandMineCell(row + 1, col - 1)) { //선택 지점의 왼쪽 아래에 지뢰가 있는지 확인
+            count++;
+        }
+        if (row + 1 < rowSize && isLandMineCell(row + 1, col)) { //선택 지점의 정아래에 지뢰가 있는지 확인
+            count++;
+        }
+        if (row + 1 < rowSize && col + 1 < colSize && isLandMineCell(row + 1, col + 1)) { //선택 지점의 오른쪽 아래에 지뢰가 있는지 확인
+            count++;
+        }
+        return count;
+    }
+
+    public void openSurroundedCells(int row, int col) {
+        if (row < 0 || row >= getRowSize() || col < 0 || col >= getColSize()) {
+            return; //전체 셀의 경계 밖이면 종료
+        }
+        if (isOpenedCell(row, col)) { // 이미 열린 셀인 경우
+            return;
+        }
+        if (isLandMineCell(row, col)) { // 지뢰셀인 경우
+            return;
+        }
+        open(row, col); // 셀을 연다.
+
+        if (doesCellHaveLandMineCount(row, col)) { // 사용자가 지뢰 개수가 표시된 셀을 선택했다면
+            return;
+        }
+
+        openSurroundedCells(row - 1, col - 1);
+        openSurroundedCells(row - 1, col);
+        openSurroundedCells(row - 1, col + 1);
+        openSurroundedCells(row, col - 1);
+        openSurroundedCells(row, col + 1);
+        openSurroundedCells(row + 1, col - 1);
+        openSurroundedCells(row + 1, col);
+        openSurroundedCells(row + 1, col + 1);
+    }
+
+    private boolean doesCellHaveLandMineCount(int row, int col) {
+        return findCell(row, col).hasLandMineCount();
+    }
+
+    private boolean isOpenedCell(int row, int col) {
+        return findCell(row, col).isOpened();
+    }
+}
